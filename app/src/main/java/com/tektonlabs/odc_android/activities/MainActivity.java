90f91a12469a25b1ac740cb454242597eab0e6df
RubyMobile <rubymobile@RubyMobiles-Mac-mini.local>
2015-05-29 11:50:14 -0500

    Fix player media

--- app/src/main/java/com/tektonlabs/odc_android/activities/MainActivity.java
+++ app/src/main/java/com/tektonlabs/odc_android/activities/MainActivity.java
@@ -3,8 +3,6 @@ package com.tektonlabs.odc_android.activities;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -41,7 +39,6 @@ import retrofit.client.UrlConnectionClient;
 import retrofit.converter.GsonConverter;
 
 public class MainActivity extends Activity {
-
     private Services services;
     private List<Media> medias;
 
@@ -55,8 +52,8 @@ public class MainActivity extends Activity {
 
     private String entity;
 
-    String SONG = "song";
-    String ALBUM = "album";
+    private String SONG = "song";
+    private String ALBUM = "album";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +80,7 @@ public class MainActivity extends Activity {
         progress.setIndeterminate(false);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.options_array, android.R.layout.simple_spinner_item);
+                                        R.array.options_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spn_options.setAdapter(adapter);
     }
@@ -210,30 +207,13 @@ public class MainActivity extends Activity {
 
     /* Acción al hacer clic en un elemento */
     public void onItemClick(int mPosition) {
-//        try {
-//            MediaPlayer player = new MediaPlayer();
-//            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//            player.setDataSource("http://a1083.phobos.apple.com/us/r1000/014/Music/v4/4e/44/b7/4e44b7dc-aaa2-c63b-fb38-88e1635b5b29/mzaf_1844128138535731917.plus.aac.p.m4a");
-//            player.prepare();
-//            player.start();
-//        } catch (Exception e) {
-//            // TODO: handle exception
-//        }
-
         Media mediaSelected = medias.get(mPosition);
-//        Gson gson = new Gson();
-//        String json = gson.toJson(tempValues);
         if (entity.equals(SONG)){
-
             openSongPlayer(mediaSelected);
-
         }else {
-
             openAlbumDetail(mediaSelected);
         }
     }
-
-
 
     /* Remover teclado */
     private void removeKeyboard(){
@@ -248,7 +228,7 @@ public class MainActivity extends Activity {
     private void openSongPlayer(Media songDetail) {
         Intent intent = new Intent(this, SongDetailActivity.class);
         intent.putExtra("ARTIST", songDetail.getArtistName());
-        intent.putExtra("SONG_NAME", songDetail.getCollectionName());
+        intent.putExtra("SONG_NAME", songDetail.getTrackName());
         intent.putExtra("PREVIEW_URL", songDetail.getPreviewUrl());
         intent.putExtra("IMAGE", songDetail.getArtworkUrl100());
         startActivity(intent);
@@ -256,8 +236,9 @@ public class MainActivity extends Activity {
 
     private void openAlbumDetail(Media albumDetail){
         Intent intent = new Intent(this, AlbumDetailActivity.class);
-
-       // intent.putExtra("ALBUM_DETAIL", albumDetail);
+        Gson gson = new Gson();
+        String json = gson.toJson(albumDetail);
+        intent.putExtra("ALBUM_DETAIL", json);
         startActivity(intent);
     }
 
--- app/src/main/java/com/tektonlabs/odc_android/activities/SongDetailActivity.java
+++ app/src/main/java/com/tektonlabs/odc_android/activities/SongDetailActivity.java
@@ -1,86 +1,84 @@
 package com.tektonlabs.odc_android.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.widget.TextViewCompat;
-import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
 import com.squareup.picasso.Picasso;
 import com.tektonlabs.odc_android.R;
 
-
-/**
- * Created by rubymobile on 5/27/15.
- */
 public class SongDetailActivity extends Activity {
     private String artist;
     private String songName;
     private String previewURL;
     private String imageURL;
 
-
     private TextView artistName;
     private TextView songname;
     private Button play;
     private Button pause;
     private ImageView imageurl;
-    MediaPlayer player = new MediaPlayer();
-
+    private MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_detail);
-        getDataMainActivity();
 
+        getDataMainActivity();
+        setupElements();
+        setupData();
+        setupMediaPlayer();
+        setupActions();
+    }
+
+    /* Seteando Elementos */
+    private void setupElements(){
         play=(Button)findViewById(R.id.play);
         pause=(Button)findViewById(R.id.pause);
 
         artistName=(TextView)findViewById(R.id.tv_artistname);
         songname=(TextView)findViewById(R.id.tv_songname);
         imageurl=(ImageView)findViewById(R.id.imageURL);
+    }
 
+    /* Setup Data*/
+    private void setupData(){
+        player = new MediaPlayer();
         artistName.setText(artist);
         songname.setText(songName);
-
-
         Picasso.with(getApplication()).load(imageURL).into(imageurl);
+    }
 
+    /* Preparando media player */
+    private void setupMediaPlayer(){
+        try {
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource(previewURL);
+            player.prepare();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 
-
+    /* Seteando Acciones a botonoes */
+    private void setupActions(){
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                try {
-                    player.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                    player.setDataSource(previewURL);
-                    player.prepare();
-                    player.start();
-
-                } catch (Exception e) {
-                    // TODO: handle exception
-                }
-
+                player.start();
             }
         });
-
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 player.pause();
             }
         });
-
-
-
-
-
     }
 
     /* Recuperar Data de la actividad main */
@@ -92,6 +90,17 @@ public class SongDetailActivity extends Activity {
             previewURL = extras.getString("PREVIEW_URL");
             imageURL = extras.getString("IMAGE");
 
+        }
+    }
+
+    /* Back pressed */
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        if (player != null) {
+            player.stop();
+            player.release();
+            player = null;
         }
     }
 }
